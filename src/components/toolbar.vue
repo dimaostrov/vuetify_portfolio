726a4cb5b8231e794a4a118bc93440d00adfc41f
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-11-12 21:16:33 -0800

    add background to parallax

---
+++ src/components/toolbar.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <v-toolbar class="white">
+        <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Toolbar',
+    data() {
+        return {
+            title: "Dmitriy Ostrovskiy"
+        }
+    }
+}
+</script>
